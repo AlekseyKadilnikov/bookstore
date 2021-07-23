b45c081932c4d2b265221d73176b61028262a8d7
Alexey Kadilnikov <kadilnikovaleksei@yandex.ru>
2021-07-23 20:44:37 +0300

    task 3-3 (add assignment)

--- task-3/3/src/product/Car.java
+++ task-3/3/src/product/Car.java
@@ -3,19 +3,25 @@ package product;
 import productpart.IProductPart;
 
 public class Car implements IProduct{
+    IProductPart body;
+    IProductPart engine;
+    IProductPart chassis;
 
     @Override
-    public void installFirstPart(IProductPart part) {
-        System.out.println("Установлена деталь: " + part.getName());
+    public void installFirstPart(IProductPart body) {
+        this.body = body;
+        System.out.println("Установлена деталь: " + body.getName());
     }
 
     @Override
-    public void installSecondPart(IProductPart part) {
-        System.out.println("Установлена деталь: " + part.getName());
+    public void installSecondPart(IProductPart chassis) {
+        this.chassis = chassis;
+        System.out.println("Установлена деталь: " + chassis.getName());
     }
 
     @Override
-    public void installThirdPart(IProductPart part) {
-        System.out.println("Установлена деталь: " + part.getName());
+    public void installThirdPart(IProductPart engine) {
+        this.engine = engine;
+        System.out.println("Установлена деталь: " + engine.getName());
     }
 }
