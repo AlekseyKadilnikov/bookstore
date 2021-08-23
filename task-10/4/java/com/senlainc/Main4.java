9495b827084d94ce40652ea2cee66dd46909ea74
Alexey Kadilnikov <kadilnikovaleksei@yandex.ru>
2021-08-23 17:45:49 +0300

    task-10 daemon

--- task-10/4/java/com/senlainc/Main4.java
+++ task-10/4/java/com/senlainc/Main4.java
@@ -1,4 +1,39 @@
 package com.senlainc;
 
+import java.time.LocalDateTime;
+
+import static java.lang.Thread.sleep;
+
 public class Main4 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ServiceThread(1));
+        thread.setDaemon(true);
+        thread.start();
+        try {
+            sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ServiceThread implements Runnable {
+
+    int interval;
+
+    public ServiceThread(int interval) {
+        this.interval = interval;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                sleep(interval * 1000);
+                System.out.println(LocalDateTime.now());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
