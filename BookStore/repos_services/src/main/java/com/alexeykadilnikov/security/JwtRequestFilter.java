bb95f807211d519087971f014df53234bddf4a48
Alexey Kadilnikov <kadilnikovaleksei@yandex.ru>
2021-10-10 17:55:50 +0300

    Task-19 security fixes

--- BookStore/repos_services/src/main/java/com/alexeykadilnikov/security/JwtRequestFilter.java
+++ BookStore/repos_services/src/main/java/com/alexeykadilnikov/security/JwtRequestFilter.java
@@ -28,34 +28,20 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         final String requestTokenHeader = request.getHeader("Authorization");
         String username = null;
         String jwtToken = null;
+
         if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
             jwtToken = requestTokenHeader.substring(7);
-            try {
-                username = jwtTokenUtil.getUsernameFromToken(jwtToken);
-            } catch (IllegalArgumentException e) {
-                throw new JwtAuthenticationException("Unable to get JWT Token", e);
-            } catch (ExpiredJwtException e) {
-                throw new JwtAuthenticationException("JWT Token has expired", e);
+            username = jwtTokenUtil.getUsernameFromToken(jwtToken);
+        }
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.jwtUserDetailsService.loadUserByUsername(username);
+            if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
-        } else {
-            logger.warn("JWT Token does not begin with Bearer String");
         }
+        chain.doFilter(request, response);
 
-
-        try {
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = this.jwtUserDetailsService.loadUserByUsername(username);
-                if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
-                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                }
-            }
-            chain.doFilter(request, response);
-        }
-        catch (Exception e) {
-            SecurityContextHolder.clearContext();
-            throw new JwtAuthenticationException("Unauthenticated");
-        }
     }
 }
